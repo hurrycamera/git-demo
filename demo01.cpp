5c42f6422224c7186f48878e8fe53e6430a6595f
hurrycamera <1480306057@qq.com>
2026-04-02 18:02:54 +0800

    secondsave

--- demo01.cpp
+++ demo01.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Welcome to C++ programming." << endl;
+    for (int i = 1; i <= 5; i++) {
+        cout << "This is line " << i << endl;
+    }
     return 0;
 }
